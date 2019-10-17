f6dc1e21488fdce6b83e1d7ef118d29dc71e1749
Howard WU <Wchoward@users.noreply.github.com>
2019-10-17 22:42:48 +0800

    k nearest neighbors done

---
+++ KNearestNeighbors/KNearestNeighbors.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from collections import Counter
+
+# data
+iris = load_iris()
+df = pd.DataFrame(iris.data, columns=iris.feature_names)
+df['label'] = iris.target
+df.columns = ['sepal length', 'sepal width', 'petal length', 'petal width', 'label']
+
+plt.scatter(df[:50]['sepal length'], df[:50]['sepal width'], label='0')
+plt.scatter(df[50:100]['sepal length'], df[50:100]['sepal width'], label='1')
+plt.xlabel('sepal length')
+plt.ylabel('sepal width')
+plt.legend()
+plt.show()
+
+data = np.array(df.iloc[:100, [0, 1, -1]])
+X, y = data[:, :-1], data[:, -1]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+
+class KNN:
+    def __init__(self, X_train, y_train, n_neighbors=3, p=2):
+        self.n = n_neighbors
+        self.p = p
+        self.X_train = X_train
+        self.y_train = y_train
+
+    def predict(self, X):
+        knn_list = []
+        for i in range(self.n):
+            dist = np.linalg.norm(X - self.X_train[i], ord=self.p)
+            knn_list.append((dist, self.y_train[i]))
+
+        for i in range(self.n, len(self.X_train)):
+            max_index = knn_list.index(max(knn_list, key=lambda x: x[0]))
+            dist = np.linalg.norm(X - self.X_train[i], ord=self.p)
+            if knn_list[max_index][0] > dist:
+                knn_list[max_index] = (dist, self.y_train[i])
+
+        knn = [k[-1] for k in knn_list]
+        count_pairs = Counter(knn)
+        max_count = sorted(count_pairs.items(), key=lambda x: x[1])[-1][0]
+        return max_count
+
+    def score(self, X_test, y_test):
+        right_count = 0
+        n = 10
+        for X, y in zip(X_test, y_test):
+            label = self.predict(X)
+            if label == y:
+                right_count += 1
+        return right_count / len(X_test)
+
+
+clf = KNN(X_train, y_train)
+score = clf.score(X_test, y_test)
+print(score)
